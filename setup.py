1e5ea36836b468202fefd828450d662015207009
Vmynt <tugrulguran@gmail.com>
2022-03-18 21:24:36 -0400

    Replaced 'py <main file>' with just 'python <main file>' and included instructions on how to configure it.

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
 from os import system
 import platform
 
-system("pip install -r requirements.txt")
+
+try:
+    import git
+    print("The 'GitPython' module is already installed.")
+
+except ModuleNotFoundError:
+    system("pip install gitpython")
 
 print()
 print()
 
 system("python GitRepoPuller.py")
+
+# If the command above does not work, you can try replacing 'python' with:
+#   + python3
+#   + py 
+# 
+# Or, you need to install python.
+#
+#
+# sudo apt install python3
+# https://python.org
